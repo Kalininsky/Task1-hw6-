da28959ea5a4eeb3a8f93e327f2e85c4ae2b42b6
Kirill <Kalininsky14@mail.ru>
2021-09-21 19:54:55 +0300

    Добавьте файлы проекта.

---
+++ Homework 6/Homework 6.cpp
@@ -0,0 +1,39 @@
+﻿
+#include <iostream>
+
+
+// task 1
+
+void separation()
+{
+	std::cout << "//===Homework===//\n\n";
+}
+
+ int main()
+{
+ 
+     separation();
+
+    int * HwArr;
+    int n;
+    std::cout << "Enter your number: ";
+    std::cin >> n;
+    HwArr = new int[n];
+    int temp = 1;
+    std::cout << "Array: ";
+    for (size_t i = 0; i < n; i++)
+    {
+        HwArr[i] = temp;
+        std::cout << HwArr[i] << " ";
+        temp *= 2;
+    }
+
+    delete[] HwArr;
+
+    return 0;
+       
+ }
+
+
+
+
